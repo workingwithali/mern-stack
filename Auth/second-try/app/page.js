bafe862922178293b99569100d04986b4326dd4d
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-21 06:55:31 +0500

    update

--- Auth/second-try/app/page.js
+++ Auth/second-try/app/page.js
@@ -1,8 +1,9 @@
-'use client'
+"use client"
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
   const { data: session } = useSession()
+  console.log(session)
   if (session) {
     return <>
       Signed in as {session.user.email} <br />
@@ -11,6 +12,7 @@ export default function Component() {
   }
   return <>
     Not signed in <br />
-    <button onClick={() => signIn()}>Sign in</button>
+    <button onClick={() => signIn("github")}>Sign in using Github</button>
+    <button onClick={() => signIn("google")}>Sign in using Google</button>
   </>
 }
