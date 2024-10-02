03ae6ea14e503a69ddac8a6c7d88ef86c57a5d50
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-02 21:48:56 +0500

    update

--- FundamwntalsOfJs/array.js
+++ FundamwntalsOfJs/array.js
@@ -1,13 +1,8 @@
 // array
 // forEach map filter indexof find
 var arr = [2,3,4,5]
-/**
- * Iterates over each element in the array and appends " hello" to each element.
- * Note: The return value of the callback function is not used by forEach.
- *
- * @param {Array} arr - The array to iterate over.
- */
-arr.forEach((var) => {
-    return var + " hello";
-});
+arr.forEach((var)=> {
+    return var +" hello"
+    
+})
 print(arr)
