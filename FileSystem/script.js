4bb8831aeac194ce10440b37801a021403d270ed
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-09 07:22:43 +0500

    update

--- FileSystem/script.js
+++ FileSystem/script.js
@@ -1,3 +1,6 @@
-const fs = require('node:fs');
+const fs = require('fs');
 
-fs.writeFile()
+fs.writeFile('ali.txt','sfdshef',function(err){
+    if (err) console.log("err");
+    else console.log("done");
+})
