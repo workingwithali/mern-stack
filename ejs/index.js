d6e048fafe0b236c2a8e789d368a1e211e4a0e4f
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-10 10:52:06 +0500

    ejs

--- ejs/index.js
+++ ejs/index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 // https://github.com/mde/ejs/wiki/Using-EJS-with-Express
+
+app.set('view engine', 'ejs');
+// app.get('/', (req, res) => {
+//     res.sendFile("templates/index.html",{root:__dirname})
+// })
 app.get('/', (req, res) => {
-    res.sendFile("templates/index.html",{root:__dirname})
-})
+    res.render('index', { foo: 'FOO' });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
