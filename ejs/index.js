8a8cfab007a1f2bbb4cd68dbec0037a767c77c10
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-10 09:41:47 +0500

    ejs

--- ejs/index.js
+++ ejs/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+// https://github.com/mde/ejs/wiki/Using-EJS-with-Express
 app.get('/', (req, res) => {
     res.sendFile("templates/index.html",{root:__dirname})
 })
