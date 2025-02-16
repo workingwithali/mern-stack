f05ab7555fef2522aad135b20f56edfbb8cb1f2c
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-16 12:20:40 +0500

    update

--- mongdb/write.mongodb.js
+++ mongdb/write.mongodb.js
@@ -4,7 +4,17 @@ db.course.insertOne({
     'price':100,
     'time':'6month'
 })
-db.course.insertMany([{
-    
-}]
-})
+db.course.insertMany(
+    [
+        { 'name': 'mongodb', 'price': 120, 'time': '6 months' },
+        { 'name': 'mongodb', 'price': 150, 'time': '8 months' },
+        { 'name': 'mongodb', 'price': 90, 'time': '5 months' },
+        { 'name': 'mongodb', 'price': 110, 'time': '7 months' },
+        { 'name': 'mongodb', 'price': 130, 'time': '6 months' },
+        { 'name': 'mongodb', 'price': 140, 'time': '9 months' },
+        { 'name': 'mongodb', 'price': 95, 'time': '4 months' },
+        { 'name': 'mongodb', 'price': 160, 'time': '10 months' },
+        { 'name': 'mongodb', 'price': 125, 'time': '6 months' },
+        { 'name': 'mongodb', 'price': 105, 'time': '5 months' }
+    ]
+)
