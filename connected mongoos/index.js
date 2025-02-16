82253920b2cd1e954c8bc45d8c911c2aabc3b770
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-16 20:34:15 +0500

    update

--- connected mongoos/index.js
+++ connected mongoos/index.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose"
 import express from 'express'
+import { Todo } from "../models/Todo.js"
+
 let conn = await mongoose.connect('mongodb://localhost:27017/todo')
 // const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+    const todo = new Tode({title:'ali', des:"this is todo",isDone:false})
+    todo.save()
     res.send('Hello World!')
 })
 
